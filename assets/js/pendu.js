0784b55d02af426430c5e0c80c1b546a0348ae52
Your Name <flocamj@gmail.com>
2022-10-24 16:22:23 +0200

    first commit

---
+++ assets/js/pendu.js
@@ -0,0 +1,10 @@
+let box = document.querySelector(".box");
+let compteur = 0
+
+let dico = ["bonjour","salut","coucou","hello"];
+
+// for (i of dico){
+//     let p = document.createElement("p")
+//     box.appendChild(p)
+//     p.textContent= i
+// }
