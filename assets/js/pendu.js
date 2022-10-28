b83c4e35e6fc0a46a0d8ba3e47405bb9c829ec0b
Your Name <flocamj@gmail.com>
2022-10-28 12:04:09 +0200

    finir le compteur d'essais

--- assets/js/pendu.js
+++ assets/js/pendu.js
@@ -7,7 +7,11 @@ let afficheAlphabet = alphabet.toUpperCase()
 let dico = ["bonjour","salut","coucou","hello"];
 let motSecret = dico[Math.floor(Math.random()*dico.length)]
 let motSecretEtoiles = []
-let essais = 0
+let essais = 7
+let sortie=false
+
+// affiche les essais restants
+p.textContent = `Il vous reste ${essais} essais`;
 
 // parcoure le tableau motSecret et crée un tableau affiché avec des "*" pour chaque lettre
 for (let i = 0; i< motSecret.length;i++){
@@ -15,10 +19,9 @@ for (let i = 0; i< motSecret.length;i++){
 }
 
 // affiche le mot secret avec des étoiles
-afficheMotSecret.textContent = motSecretEtoiles.join("");
-p.textContent = `Il vous reste ${essais} essais`;
+afficheMotSecret.textContent = `Le mot secret est : ${motSecretEtoiles.join("")}`;
 
-// crée les buttons de l'alphabet
+// crée les buttons de l'alphabet et vérifie 
 for (let i = 0; i<afficheAlphabet.length;i++){
     const button = document.createElement("button");
     button.className = "lettres";
@@ -26,16 +29,17 @@ for (let i = 0; i<afficheAlphabet.length;i++){
     button.textContent+=afficheAlphabet.charAt(i);
     button.value+=alphabet.charAt(i);
 
+    // récupère la valeur de la lettre du boutton pour vérifier si il est dans le motSecret
     button.addEventListener("click",()=>{
-        for (let i=0; i<alphabet;i++){
-            console.log(motSecretEtoiles[i]);
-            if(button.value===alphabet.charAt(i)){
+        for (let i=0; i<alphabet.length;i++){
+            if(button.value===motSecret[i]){
                 motSecretEtoiles[i]=button.value;
+                button.style="visibility: hidden;";
+                afficheMotSecret.textContent += `Le mot secret est : ${motSecretEtoiles.join("")}`;
             }
-            console.log(alphabet.charAt(i));
-            console.log(button.value);
+            else{
+                afficheMotSecret.textContent += `Il vous reste ${essais-1} essais`;
+            }
         }
-        button.disabled="disabled";
-        button.style="background-color:grey;"
     })
 }
